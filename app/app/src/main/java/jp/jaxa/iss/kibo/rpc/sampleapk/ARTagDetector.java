0841124a6d94a1c36882ace40a27924fecc6af7d
zebra314 <kidalfonso@gmail.com>
2025-05-10 01:55:35 +0200

    Update to enable detect multiple tags in single image

--- app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/ARTagDetector.java
+++ app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/ARTagDetector.java
@@ -48,94 +48,115 @@ public class ARTagDetector {
     Log.i(TAG, "Initialized");
   }
 
-  public Mat getclippedImage(Mat undistortedImage) {
-    Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-    ArrayList<Mat> corners = new ArrayList<>();
-    Mat markerIds = new Mat();
-
-    Aruco.detectMarkers(undistortedImage, dictionary, corners, markerIds);
-    if (corners.isEmpty()) 
-      return null;
-
-    Mat clippedImage = clip(undistortedImage, corners.get(0));
-    return clippedImage;
-  }
-
-  public Map detect(Mat undistortedImage) {
+  /**
+   * Detect AR tags in the given image.
+   * 
+   * @param undistortedImage The input image.
+   * @return A list of detected AR tags.
+   */
+  public List<ARTag> detect(Mat undistortedImage) {
     List<Mat> corners = new ArrayList<>();
     Mat markerIds = new Mat();
+
     Aruco.detectMarkers(undistortedImage, arucoDictionary, corners, markerIds);
   
-    Map<Integer, Pose> result = new HashMap<>();
-
     if (markerIds.empty()) {
-      Log.w(TAG, "No AR Tag Found");
-      return result;
+      Log.w(TAG, "No AR Tag Found.");
+      return new ArrayList<>();
     }
 
-    Mat rvec = new Mat();
-    Mat tvec = new Mat();
-
+    List<ARTag> results = new ArrayList<>();
     for (int i = 0; i < markerIds.rows(); i++) {
-      // Get marker ID
-      int id = (int) markerIds.get(i, 0)[0];
-
-      // Get corner 
+      int markerId = (int) markerIds.get(i, 0)[0];
       Mat corner = corners.get(i);
+      ARTag arTag = new ARTag(markerId, corner);
+      results.add(arTag);
+    }
+
+    return results;
+  }
+
+  public Map<Integer, Pose> filterResult(List<ARTag> arResults, Pose currentPose) {
+    Map<Integer, Pose> tagPoses = new HashMap<>();
+
+    for (ARTag arTag : arResults) {
+      int markerId = arTag.getMarkerId();
+      Mat corner = arTag.getCorner();
+
+      Mat rvec = new Mat();
+      Mat tvec = new Mat();
 
       // Estimate pose
       Aruco.estimatePoseSingleMarkers(Collections.singletonList(corner), markerLength, cameraMatrix, distortionCoefficients, rvec, tvec);
 
       // Extract shift matrix
       double[] t = tvec.get(0, 0);
-      Point position = new Point(t[0], t[1], t[2]);
+      Point point = new Point(t[0], t[1], t[2]);
 
       // Extract rotation matrix
       double[] r = rvec.get(0, 0);
       Mat rotMat = new Mat();
       Calib3d.Rodrigues(new MatOfDouble(r), rotMat);
+      Quaternion quaternion = new Quaternion(); // rotationMatrixToQuaternion(rotMat); this param is never used, so keep it simple.
 
-      Quaternion orientation = rotationMatrixToQuaternion(rotMat);
-
-      result.put(id, new Pose(position, orientation));
+      Pose tagPoseCamera = new Pose(point, quaternion);
+      Pose tagPoseWorld = convertCameraToWorld(tagPoseCamera, currentPose);
+      tagPoses.put(markerId, tagPoseWorld);
     }
 
-    return result;
+    return tagPoses;
   }
 
-  public Pose filterResult(Map<Integer, Pose> arResult, int areaId, Pose currentPose) {
-    Pose tagPose = null;
-    Log.i(TAG, "Current Body Pose in World: " + currentPose.toString());
-    for (Map.Entry<Integer, Pose> entry : arResult.entrySet()) {
-      Integer id = entry.getKey();
-      Pose pose = entry.getValue();
-      Pose poseWorld = convertCameraToWorld(pose, currentPose);
-      Log.i(TAG, "ID: " + id + ", Pose in poseWorld: " + poseWorld.toString());
+  public Pose guessResult(int areaId) {
+    Log.w(TAG, "No correct tag found, set tagPose to the center of this area.");
+    
+    Pose tagPose; 
 
-      if(id % 100 == areaId) {
-        tagPose = poseWorld;
-      }
-    }
-
-    if (tagPose == null) {
-      Log.w(TAG, "No correct tag found, set tagPose to the center of this area.");
-
-      if (areaId == 1) {
-        tagPose = new Pose(new Point((10.42 + 11.48) / 2, -10.58, (4.82 + 5.57) / 2), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
-      } else if (areaId == 2) {
-        tagPose = new Pose(new Point((10.3 + 11.55) / 2, (-9.25 + -8.5) / 2, 3.76203), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
-      } else if (areaId == 3) {
-        tagPose = new Pose(new Point((10.3 + 11.55) / 2, (-8.4 + -7.45) / 2, 3.76203), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
-      } else if (areaId == 4) {
-        tagPose = new Pose(new Point(9.866984, (-7.34 + -6.365) / 2, (4.32 + 5.57) / 2), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
-      } else if (areaId == 0) {
-        tagPose = new Pose();
-      } else {
-        Log.i(TAG, "Area id not valid.");
-      }
+    if (areaId == 1) {
+      tagPose = new Pose(new Point((10.42 + 11.48) / 2, -10.58, (4.82 + 5.57) / 2), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
+    } else if (areaId == 2) {
+      tagPose = new Pose(new Point((10.3 + 11.55) / 2, (-9.25 + -8.5) / 2, 3.76203), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
+    } else if (areaId == 3) {
+      tagPose = new Pose(new Point((10.3 + 11.55) / 2, (-8.4 + -7.45) / 2, 3.76203), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
+    } else if (areaId == 4) {
+      tagPose = new Pose(new Point(9.866984, (-7.34 + -6.365) / 2, (4.32 + 5.57) / 2), new Quaternion(1.0f, 0.0f, 0.0f, 0.0f));
+    } else if (areaId == 0) {
+      tagPose = new Pose();
+    } else {
+      tagPose = new Pose();
+      Log.i(TAG, "Area id not valid.");
     }
 
     return tagPose;
+  }
+
+  /**
+   * Get the clipped image of the detected AR tag.
+   * 
+   * @param undistortedImage The input image.
+   * @return The clipped image.
+   */
+  public Map<Integer, Mat> getclippedImages(List<ARTag> arResults, Mat undistortedImage) {
+    if (arResults.isEmpty()) {
+      Log.w(TAG, "No AR Tag Found.");
+      return null;
+    }
+
+    Map<Integer, Mat> clippedImages = new HashMap<>();
+    for(ARTag arTag : arResults) {
+      int markerId = arTag.getMarkerId();
+      Mat corner = arTag.getCorner();
+
+      // Clip the image
+      Mat clippedImage = clip(undistortedImage, corner);
+      if (clippedImage.empty()) {
+        Log.w(TAG, "Clipped image is empty.");
+        continue;
+      }
+
+      clippedImages.put(markerId, clippedImage);
+    }
+    return clippedImages;
   }
 
   /* ----------------------------- Tool Functions ----------------------------- */
@@ -309,9 +330,21 @@ public class ARTagDetector {
     return new Quaternion(x, y, z, w);
   }
 
+
+  /**
+   * Clip the image to the area of interest.
+   * 
+   * @param image The input image.
+   * @param corner The corners of the area of interest.
+   * @return The clipped image.
+   */
   private Mat clip(Mat image, Mat corner) {
     final org.opencv.core.Point[] points = new org.opencv.core.Point[4];
-    // point[0]:ARコードの左上の座標, point[1]:右上の座標, point[2]:右下の座標, point[3]:左下の座標
+
+    // point[0] left top
+    // point[1] right top
+    // point[2] right bottom
+    // point[3] left bottom
     for (int i = 0; i < 4; i++) {
       points[i] = new org.opencv.core.Point(corner.get(0, i));
     }
@@ -320,6 +353,7 @@ public class ARTagDetector {
     double[] corner2 = corner.get(0, 2);
     double[] corner3 = corner.get(0, 3);
 
+    // Calculate the width and height of the rectangle
     points[0] = new org.opencv.core.Point(-103 / 20 * (corner1[0] - corner0[0]) - 5 / 4 * (corner3[0] - corner0[0]) + corner0[0], -103 / 20 * (corner1[1] - corner0[1]) - 5 / 4 * (corner3[1] - corner0[1]) + corner0[1]);
     points[1] = new org.opencv.core.Point(17 / 20 * (corner1[0] - corner0[0]) - 5 / 4 * (corner3[0] - corner0[0]) + corner0[0], 17 / 20 * (corner1[1] - corner0[1]) - 5 / 4 * (corner3[1] - corner0[1]) + corner0[1]);
     points[2] = new org.opencv.core.Point(17 / 20 * (corner1[0] - corner0[0]) + 15 / 4 * (corner3[0] - corner0[0]) + corner0[0], 17 / 20 * (corner1[1] - corner0[1]) + 15 / 4 * (corner3[1] - corner0[1]) + corner0[1]);
@@ -328,10 +362,11 @@ public class ARTagDetector {
     double width = Math.sqrt(Math.pow(points[0].x - points[1].x, 2) + Math.pow(points[0].y - points[1].y, 2));
     double height = Math.sqrt(Math.pow(points[0].x - points[3].x, 2) + Math.pow(points[0].y - points[3].y, 2));
 
+    // Create a transformation matrix
     Mat transformMatrix;
     {
       MatOfPoint2f srcPoints = new MatOfPoint2f(points);
-      srcPoints.convertTo(srcPoints, CvType.CV_32F); // タイプをCV_32Fに変換
+      srcPoints.convertTo(srcPoints, CvType.CV_32F);
 
       MatOfPoint2f dstPoints = new MatOfPoint2f(
         new org.opencv.core.Point(0, 0),
@@ -339,12 +374,15 @@ public class ARTagDetector {
         new org.opencv.core.Point(width - 1, height - 1),
         new org.opencv.core.Point(0, height - 1)
       );
-      dstPoints.convertTo(dstPoints, CvType.CV_32F); // タイプをCV_32Fに変換
 
-      // 変換前の座標と変換後の座標から透視変換行列(切り抜きたい領域を長方形に変換するための行列)を作る
+      // Convert to float
+      dstPoints.convertTo(dstPoints, CvType.CV_32F);
+
+      // Get the perspective transform matrix
       transformMatrix = Imgproc.getPerspectiveTransform(srcPoints, dstPoints);
     }
 
+    // Apply the perspective transformation
     Mat clippedImage = Mat.zeros((int) height, (int) width, image.type());
     Imgproc.warpPerspective(image, clippedImage, transformMatrix, clippedImage.size());
 
