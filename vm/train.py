332e462279eb6cc345b364b1dd0a0e3f8fe4f89d
zebra314 <kidalfonso@gmail.com>
2025-05-15 03:40:03 +0200

    Update training script

--- vm/train.py
+++ vm/train.py
@@ -3,13 +3,12 @@ import os
 from pathlib import Path
 from data_generator import DataGenerator
 
-images_per_run = 10
-total_run = 5
-epoches_per_run = 2
+total_images = 20000
+epoches = 100
 
 data_generator = DataGenerator()
 data_generator.generate_yaml()
-data_generator.generate_data(images_per_run)
+data_generator.generate_data(total_images)
 data_generator.split_data()
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,34 +20,15 @@ if not DATA_YAML.exists():
   print(f"Dataset YAML file not found at {DATA_YAML.resolve()}")
   exit(1)
 
-# First run
 model_path = os.path.join(base_dir, "yolo11m.pt")
-model = YOLO(model = model_path)
 
+model = YOLO()
 results = model.train(
   data=DATA_YAML,
-  epochs=epoches_per_run,
+  epochs=epoches,
   imgsz=640,
+  batch=-1,
+  device=0,
+  plots=True,
 )
-
-results = model.val(
-  data = DATA_YAML,
-)
-
-for i in range(total_run):
-  data_generator.generate_data(images_per_run)
-  data_generator.split_data()
-
-  model = YOLO(model_path)
-
-  results = model.train(
-    data=DATA_YAML,
-    epochs=epoches_per_run,
-    imgsz=640,
-  )
-
-  results = model.val(
-    data = DATA_YAML,
-  )
-
-model.export(format = "tflite")
+model.save(model_path)
