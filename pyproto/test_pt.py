1ed7184a5951ec3fae25e10509b9f9bfdbbdc6ad
zebra314 <kidalfonso@gmail.com>
2025-05-16 12:15:13 +0200

    Update test script

--- pyproto/test_pt.py
+++ pyproto/test_pt.py
@@ -1,25 +1,20 @@
 from ultralytics import YOLO
+from pathlib import Path
 
 # Folders
-test_set_dir = '../assets/test_set'
-test_results_dir = '../assets/test_results'
+base_dir = Path(__file__).resolve().parent
+test_set_dir = base_dir / '../assets/test_set'
+test_results_dir = base_dir / '../assets/test_result'
+
+test_set_dir = test_set_dir.resolve()
+test_results_dir = test_results_dir.resolve()
 
 # Load a model
-model = YOLO('yolo11s.pt')
+model = YOLO(base_dir / 'yolo11s.pt')
 
 # Run batched inference on a list of images
 test_set = []
-for i in range(1, 6):
-  test_set.append(f"{test_set_dir}/{i}.png")
+for i in range(1, 8):
+  test_set.append(test_set_dir / f"{i}.png")
 
-results = model(test_set)
-
-# Process results list
-for i, result in enumerate(results):
-  boxes = result.boxes
-  masks = result.masks
-  keypoints = result.keypoints
-  probs = result.probs
-  obb = result.obb
-  result.show()
-  result.save(filename=f"{i}.png")
+results = model.predict(test_set, save=False, conf=0.5, imgsz=640)
