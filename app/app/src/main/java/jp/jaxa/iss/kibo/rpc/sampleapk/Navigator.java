36ea0bf229538047c4114073bed8a635d696a906
jonathan0713 <jonathan0713tw@gmail.com>
2025-05-24 22:27:22 +0800

    Update navigator.py Navigator.java, new phtotgraph point

--- app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Navigator.java
+++ app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Navigator.java
@@ -29,9 +29,11 @@ public class Navigator {
     areaPoses.put(1, new Pose(new Point(10.95, -9.49, 5.435), new Quaternion(0.0f, 0.0f, -0.707f, 0.707f)));      // Area 1
     areaPoses.put(2, new Pose(new Point(10.925, -8.6, 4.55), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)));      // Area 2
     areaPoses.put(3, new Pose(new Point(10.925, -7.925, 4.462), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)));      // Area 3
-    areaPoses.put(4, new Pose(new Point(11.2, -6.853, 4.945), new Quaternion(0.0f, -0.707f, 0.707f, 0.0f)));     // Area 4
+    // areaPoses.put(4, new Pose(new Point(11.2, -6.853, 4.945), new Quaternion(0.0f, -0.707f, 0.707f, 0.0f)));     // Area 4
+    areaPoses.put(4, new Pose(new Point(10.965, -7.5, 5.25), new Quaternion(0.0f, -0.707f, 0.707f, 0.0f)));     // Area 4
+
     areaPoses.put(5, new Pose(new Point(10.935, -8.55, 4.955), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f))); // Combined Area 2 3
-    areaPoses.put(0, new Pose(new Point(11.143, -6.7607, 4.935), new Quaternion(-0.5f, -0.5f, 0.5f, 0.5f))); // Report
+    areaPoses.put(0, new Pose(new Point(11.283, -6.7607, 4.935), new Quaternion(-0.5f, -0.5f, 0.5f, 0.5f))); // Report
   }
 
   // Safety factors
@@ -192,7 +194,7 @@ public class Navigator {
 
     // Wait Stable
     try{
-      Thread.sleep(800);
+      Thread.sleep(600);
     } catch (InterruptedException e) {
       Log.w(TAG, "Fail to sleep thread" + e);
     }
