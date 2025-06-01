9cf303dab17e727439acebfa9a876f2097dbc32c
zebra314 <kidalfonso@gmail.com>
2025-06-01 13:23:12 +0200

    Update poese and stable time

--- app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Navigator.java
+++ app/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Navigator.java
@@ -29,9 +29,9 @@ public class Navigator {
     areaPoses.put(1, new Pose(new Point(11.11, -9.49, 5.435), new Quaternion(0.0f, 0.0f, -0.707f, 0.707f)));          // Area 1
     areaPoses.put(2, new Pose(new Point(10.925, -8.6, 4.55), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)));               // Area 2
     areaPoses.put(3, new Pose(new Point(10.925, -7.925, 4.462), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)));            // Area 3
-    areaPoses.put(4, new Pose(new Point(11.35, -6.7607, 4.935), new Quaternion(0.0f, 0.707f, -0.707f, 0.0f)));        // Area 4
-    areaPoses.put(5, new Pose(new Point(10.925, -8.35, 5.2), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)));               // Combined Area 2 3
-    areaPoses.put(0, new Pose(new Point(11.35, -6.7607, 4.935), new Quaternion(0.633f, 0.754f, -0.133f, 0.112f)));    // Report
+    areaPoses.put(4, new Pose(new Point(11.35, -6.76, 4.935), new Quaternion(0.0f, -1.0f, 0.0f, 0.0f)));              // Area 4
+    areaPoses.put(5, new Pose(new Point(10.925, -8.35, 5.33), new Quaternion(0.0f, 0.707f, 0.0f, 0.707f)));            // Combined Area 2 3
+    areaPoses.put(0, new Pose(new Point(11.35, -6.76, 4.935), new Quaternion(0.633f, 0.754f, -0.133f, 0.112f)));      // Report
   }
 
   // Safety factors
@@ -181,16 +181,35 @@ public class Navigator {
       return null;
     }
 
+    Log.i(TAG, "Start moving to area " + area);
     Result result = moveTo(targetPose);
+
+    int stableTime = 0;
+    switch (area) {
+      case 0:
+        stableTime = 700;
+        break;
+      case 1:
+        stableTime = 1000;
+        break;
+      case 4:
+        stableTime = 1500;
+        break;
+      case 5:
+        stableTime = 2400;
+        break;
+      default:
+        stableTime = 700;
+        break;
+    }
 
     // Wait Stable
     try{
-      Thread.sleep(800);
+      Thread.sleep(stableTime);
     } catch (InterruptedException e) {
       Log.w(TAG, "Fail to sleep thread" + e);
     }
     
-    Log.i(TAG, "Move to area " + area);
     return result;
   }
 
