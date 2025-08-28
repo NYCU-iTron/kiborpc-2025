0cb89fb9e0f2f772c692c1e97eb2e084dd2ae944
zebra314 <kidalfonso@gmail.com>
2025-08-29 00:11:23 +0800

    Update test data_config script

--- vm/scripts/test_data_config.py
+++ vm/scripts/test_data_config.py
@@ -1,15 +1,22 @@
+import logging
 from data_generator import DataGenerator
-from config.data_config import DataConfig
-from config.data_config import ItemType
+from config.data_config import ItemType, DataConfig
 from config.logging_config import setup_logging
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
     setup_logging()
     generator = DataGenerator()
 
     config = DataConfig(
-        item_list=[ItemType.coin] * 3,
+        item_list=[ItemType.compass] * 4,
+        image_size=(200, 200),
+        force_overlap=False,
+        apply_shear=False,
+        max_overlap=0.3,
     )
 
     data = generator.generate_single_data(config)
+    logger.info(data.to_string())
     data.plot()
