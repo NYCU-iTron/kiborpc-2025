fa231efd748e9b380e1101c77daf0f55b0261ed4
zebra314 <kidalfonso@gmail.com>
2025-08-30 05:42:57 +0800

    Add script to generate a small random dataset

---
+++ vm/scripts/test_random_small_dataset.py
@@ -0,0 +1,17 @@
+import logging
+from data_generator import DataGenerator
+from config.generator_config import GeneratorConfig
+from config.logging_config import setup_logging
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    setup_logging()
+    generator = DataGenerator()
+
+    config = GeneratorConfig(
+        single_item_images_num=10,
+        multi_item_images_num=100,
+    )
+
+    generator.generate_dataset(config)
