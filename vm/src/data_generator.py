a03a4daa68525bc7fae1e603b851a345be6a6ec8
zebra314 <kidalfonso@gmail.com>
2025-11-04 12:02:46 +0800

    Update dataset generation logic

--- vm/src/data_generator.py
+++ vm/src/data_generator.py
@@ -16,14 +16,14 @@ class DataGenerator:
         self.item_image_dir = Path(__file__).parent.parent / "item_images"
         if not self.item_image_dir.is_dir():
             raise FileNotFoundError(f"Item image directory not found: {self.item_image_dir}")
-        
+
         # Load item images, accept only webp
         self.item_dict = {}
         for item_type in ItemType:
             item_image_file = self.item_image_dir / f"{item_type.name}.webp"
             if not item_image_file.is_file():
                 raise FileNotFoundError(f"Item image not found: {item_image_file}")
-        
+
             item_image = Image.open(item_image_file).convert("RGBA")
             self.item_dict[item_type] = np.array(item_image)
             self.logger.info(f"Loaded item image: name='{item_type.name}.webp', id={item_type.value}")
@@ -36,7 +36,7 @@ class DataGenerator:
         for split in DatasetSplit:
             (self.images_dir / split.name).mkdir(parents=True, exist_ok=True)
             (self.labels_dir / split.name).mkdir(parents=True, exist_ok=True)
-        
+
         self.logger.info(f"Prepared dataset directories under: {self.dataset_dir}")
 
         # Generate yaml file
@@ -45,7 +45,7 @@ class DataGenerator:
             f.write(
                 f"path: {self.dataset_dir.resolve()}\n"
                 f"train: images/train\n"
-                f"val: images/val\n\n"
+                f"val: images/valid\n\n"
                 f"names:\n"
             )
             for item_type in ItemType:
@@ -61,8 +61,10 @@ class DataGenerator:
             self.logger.info(f"Random seed set to: {config.seed}")
 
         # Generate plane image
-        image_size = config.image_size
-        image = np.ones((image_size[1], image_size[0], 3), dtype=np.uint8) * 255
+        if config.image_shape is None:
+            length = random.randint(config.image_shape_range[0], config.image_shape_range[1])
+            config.image_shape = (length, length)
+        image = np.ones((config.image_shape[1], config.image_shape[0], 3), dtype=np.uint8) * 255
 
         bboxes = []
         for item in config.item_list:
@@ -70,7 +72,7 @@ class DataGenerator:
 
             # Randomly scale item image
             scale = random.uniform(config.item_scale_range[0], config.item_scale_range[1])
-            target_width = int(config.image_size[0] * scale)
+            target_width = int(image.shape[1] * scale)
             scale = target_width / item_image.shape[1]
             item_image = self.scale_image(item_image, scale)
 
@@ -79,31 +81,31 @@ class DataGenerator:
             item_image = self.rotate_image(item_image, angle)
 
             # Blur item image
-            if config.item_blur_kernel:
-                item_blur_kernel = config.item_blur_kernel
-            else:
-                item_blur_kernel = random.choice(config.item_blur_kernel_range) 
-                config.item_blur_kernel = item_blur_kernel
+            item_image = cv2.GaussianBlur(item_image, (1, 1), 0)
 
-            item_image[:, :, :3] = cv2.GaussianBlur(item_image[:, :, :3], (item_blur_kernel, item_blur_kernel), 0)
+            # Avoid very dark item images
+            black_limit = 80
+            item_rgb = item_image[:, :, :3].astype(np.float32)
+            item_rgb = np.where(item_rgb < black_limit, black_limit, item_rgb)
+            item_image[:, :, :3] = item_rgb.astype(np.uint8)
 
             # Randomly place item image on background
             item_height, item_width = item_image.shape[:2]
             success = False
             for trial in range(config.max_random_placement_trials):
-                x = random.randint(0, max(0, config.image_size[0] - item_width))
-                y = random.randint(0, max(0, config.image_size[1] - item_height))
+                x = random.randint(0, max(0, image.shape[1] - item_width))
+                y = random.randint(0, max(0, image.shape[0] - item_height))
                 new_bbox = (x, y, x + item_width, y + item_height)
 
                 # Make sure the item is within the image bounds
-                if not (x >= 0 and y >= 0 and x + item_width <= config.image_size[0] and y + item_height <= config.image_size[1]):
+                if not (x >= 0 and y >= 0 and x + item_width <= image.shape[1] and y + item_height <= image.shape[0]):
                     continue
 
                 # Check for overlap with existing items
                 if len(bboxes) > 0:
                     ious = [self.calc_bbox_iou(new_bbox, bbox) for bbox in bboxes]
                     overlaps = [self.calc_bbox_max_overlap_ratio(new_bbox, bbox) for bbox in bboxes]
-                    
+
                     max_iou = max(ious)
                     max_overlap = max(overlaps)
 
@@ -128,46 +130,54 @@ class DataGenerator:
             if not success:
                 self.logger.warning(f"Failed to place item '{item.name}' after {config.max_random_placement_trials} trials.")
                 continue
-        
+
         if len(bboxes) == 0:
             self.logger.warning("No items placed on the image.")
             return None
-        
+
         # Apply shear operation
         if config.apply_shear:
-            if config.shear:
-                shear_factor = config.shear
-            else:
-                shear_factor = random.uniform(config.shear_range[0], config.shear_range[1])
-                config.shear = shear_factor
+            if config.shear is None:
+                config.shear = random.uniform(config.shear_range[0], config.shear_range[1])
+            image, bboxes = self.shear_image(image, bboxes, config.shear)
 
-            image, bboxes = self.shear_image(image, bboxes, shear_factor)
-        
         # Apply brightness/contrast adjustment
-        if config.brightness:
-            brightness_factor = config.brightness
-        else:
-            brightness_factor = random.uniform(config.brightness_range[0], config.brightness_range[1])
-            config.brightness = brightness_factor
-        
-        if config.contrast:
-            contrast_factor = config.contrast
-        else:
-            contrast_factor = random.uniform(config.contrast_range[0], config.contrast_range[1])
-            config.contrast = contrast_factor
+        image_rgb = image.astype(np.float32)
+        black_limit = 30
+        image_rgb = np.where(image_rgb < black_limit, black_limit, image_rgb)
 
-        image = cv2.convertScaleAbs(image, alpha=contrast_factor, beta=(brightness_factor - 1) * 128)
-        
+        if config.brightness is None:
+            config.brightness = random.uniform(config.brightness_range[0], config.brightness_range[1])
+        brightness_factor = config.brightness
+        image_rgb = np.clip(image_rgb * brightness_factor, 0, 255)
+        image = image_rgb.astype(np.uint8)
+
+        # Blur image
+        kernel_size = 1
+        if image.shape[0] >= 150 or image.shape[1] >= 150:
+            kernel_size = 5
+        elif image.shape[0] >= 100 or image.shape[1] >= 100:
+            kernel_size = 3
+        image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+        # Add gaussian noise
+        if config.noise_std is None:
+            config.noise_std = random.uniform(config.noise_std_range[0], config.noise_std_range[1])
+
+        noise = np.random.normal(0, config.noise_std, image.shape[:2]).astype(np.float32)
+        noise = np.repeat(noise[:, :, np.newaxis], 3, axis=2)
+        image = image.astype(np.float32) + noise
+        image = np.clip(image, 0, 255).astype(np.uint8)
+
         # Prepare annotations
         annotations = []
-        image_width, image_height = config.image_size
         for item, bbox in zip(config.item_list, bboxes):
             x1, y1, x2, y2 = bbox
             xc, yc = (x1 + x2) / 2, (y1 + y2) / 2
             item_width, item_height = x2 - x1, y2 - y1
-            annotation = (item, xc/image_width, yc/image_height, item_width/image_width, item_height/image_height)
+            annotation = (item, xc/image.shape[1], yc/image.shape[0], item_width/image.shape[1], item_height/image.shape[0])
             annotations.append(annotation)
-        
+
         # Prepare Data object
         data = Data(
             image=image,
@@ -178,7 +188,7 @@ class DataGenerator:
         )
 
         return data
-    
+
     def generate_dataset(self, config: GeneratorConfig) -> None:
         if config.seed is not None:
             random.seed(config.seed)
@@ -197,11 +207,9 @@ class DataGenerator:
         # Generate data configs for single item images
         for item in landmark_items:
             for _ in range(config.single_item_images_num):
-                image_size = random.randint(config.image_size_range[0], config.image_size_range[1])
                 num_repeats = random.choices(config.single_item_num_choices, weights=config.single_item_num_weights)[0]
                 data_config = DataConfig(
                     item_list=[item] * num_repeats,
-                    image_size=(image_size, image_size),
                     force_overlap=True if random.random() < config.single_force_overlap_ratio else False,
                     max_overlap=config.single_force_max_overlap,
                 )
@@ -210,7 +218,7 @@ class DataGenerator:
 
         single_item_count = len(data_config_list)
         self.logger.info(f"Generated {single_item_count} single item image configs.")
-        
+
         # Generate data configs for multi item images
         for _ in range(config.multi_item_images_num):
             item_num = random.choices(config.multi_item_num_choices, weights=config.multi_item_num_weights)[0]
@@ -223,28 +231,26 @@ class DataGenerator:
                 item_list.append(treasure_item)
                 item_num -= 1
                 item_type_num -= 1
-                
+
             item_pool = random.sample(landmark_items, k=item_type_num)
             item_list += random.choices(item_pool, k=item_num)
-            image_size = random.randint(config.image_size_range[0], config.image_size_range[1])
-            
+
             data_config = DataConfig(
                 item_list=item_list,
-                image_size=(image_size, image_size),
                 force_overlap=True if random.random() < config.multi_force_overlap_ratio else False,
                 max_overlap=config.multi_force_max_overlap,
             )
             data_config_list.append(data_config)
             for item in item_list:
                 type_count[item] += 1
-        
+
         multi_item_count = len(data_config_list) - single_item_count
         self.logger.info(f"Generated {multi_item_count} multi item image configs.")
-        
+
         self.logger.info(f"Total {len(data_config_list)} image configs generated.")
         for item, count in type_count.items():
             self.logger.info(f"  Item '{item.name}': {count} instances.")
-        
+
         # Shuffle data configs
         random.shuffle(data_config_list)
         total_images = len(data_config_list)
@@ -286,11 +292,11 @@ class DataGenerator:
     # ------------------------------ Tool Functions ------------------------------ #
     def scale_image(self, image: np.ndarray, scale: float) -> np.ndarray:
         new_size = (int(image.shape[1] * scale), int(image.shape[0] * scale))
-        
+
         if scale < 1.0:
             interp = cv2.INTER_AREA
         else:
-            interp = cv2.INTER_CUBIC 
+            interp = cv2.INTER_CUBIC
 
         image = cv2.resize(image, new_size, interpolation=interp)
         return image
@@ -299,41 +305,41 @@ class DataGenerator:
         # Compute the center of the image
         height, width = image.shape[:2]
         center = (width // 2, height // 2)
-        
+
         # Compute the bounding box of the rotated image
         cos = np.abs(np.cos(np.radians(angle)))
         sin = np.abs(np.sin(np.radians(angle)))
         new_width = int((height * sin) + (width * cos))
         new_height = int((height * cos) + (width * sin))
-        
+
         # Compute the rotation matrix
         rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
         rotation_matrix[0, 2] += (new_width / 2) - center[0]
         rotation_matrix[1, 2] += (new_height / 2) - center[1]
-        
+
         # Rotate the image
-        image = cv2.warpAffine(image, 
-                               rotation_matrix, 
+        image = cv2.warpAffine(image,
+                               rotation_matrix,
                                (new_width, new_height),
                                flags=cv2.INTER_CUBIC,
                                borderMode=cv2.BORDER_CONSTANT,
                                borderValue=(0, 0, 0, 0))
-        
+
         # Crop the image to the bounding box
         alpha = image[:, :, 3]
         mask = (alpha > 0).astype(np.uint8)
         x, y, w, h = cv2.boundingRect(mask)
         image = image[y:y+h, x:x+w]
-        
+
         return image
 
     def shear_image(self, image: np.ndarray, bboxes: list[tuple[float, float, float, float]], shear_factor: float) -> tuple[np.ndarray, list]:
         shear_matrix = np.array([[1, shear_factor, 0], [0, 1, 0]], dtype=np.float32)
-        
+
         height, width = image.shape[:2]
         is_rgba = image.shape[2] == 4
         image_rgb_part = image[:,:,:3]
-        
+
         alpha_part_sheared = None
 
         # Shear alpha channel
@@ -347,11 +353,11 @@ class DataGenerator:
         new_bboxes = []
         for bbox in bboxes:
             x1, y1, x2, y2 = bbox
-            
+
             corners = np.array([
-                [x1, y1], [x2, y1], 
+                [x1, y1], [x2, y1],
                 [x1, y2], [x2, y2]])
-            
+
             # Apply shear transformation to each corner: x_new = x_old + shear_factor * y_old; y_new = y_old
             transformed_corners = np.array([
                 [c[0] + shear_factor * c[1], c[1]] for c in corners
@@ -370,7 +376,7 @@ class DataGenerator:
             new_y2 = np.clip(new_y2, 0, height)
 
             # Ensure x1 <= x2 and y1 <= y2
-            if new_x1 > new_x2: new_x1, new_x2 = new_x2, new_x1 
+            if new_x1 > new_x2: new_x1, new_x2 = new_x2, new_x1
             if new_y1 > new_y2: new_y1, new_y2 = new_y2, new_y1
 
             new_bboxes.append((new_x1, new_y1, new_x2, new_y2))
@@ -381,7 +387,7 @@ class DataGenerator:
         # Reconstruct final image
         if is_rgba and alpha_part_sheared is not None:
             new_image = cv2.merge((new_image, alpha_part_sheared))
-        
+
         return new_image, new_bboxes
 
     def calc_bbox_iou(self, box1, box2) -> float:
@@ -395,7 +401,7 @@ class DataGenerator:
         inter_width = max(xi2 - xi1, 0)
         inter_height = max(yi2 - yi1, 0)
         inter_area = inter_width * inter_height
-        
+
         # Calculate union
         box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
         box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
@@ -403,9 +409,9 @@ class DataGenerator:
 
         if union_area == 0:
             return 0.0
-        
+
         return inter_area / union_area
-    
+
     def calc_bbox_max_overlap_ratio(self, box1, box2) -> float:
         # box = (x1, y1, x2, y2)
         xi1 = max(box1[0], box2[0])
@@ -417,12 +423,12 @@ class DataGenerator:
         inter_width = max(xi2 - xi1, 0)
         inter_height = max(yi2 - yi1, 0)
         inter_area = inter_width * inter_height
-        
+
         # Calculate areas
         box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
         box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
 
         if box1_area == 0 or box2_area == 0:
             return 0.0
-        
+
         return inter_area / min(box1_area, box2_area)
